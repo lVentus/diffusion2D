c9a483a5ee957b36a42848ac67884af745e0d3bc
lVentus <st181070@stud.uni-stuttgart.de>
2024-11-17 23:32:49 +0100

    trying versioneer

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ import versioneer
 from setuptools import setup
 
 setup(
+    name="guanpu_diffusion2d",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    packages=["guanpu_diffusion2d"],
+    install_requires=["numpy", "matplotlib"],
 )
